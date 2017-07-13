fdd68b417b0eacc9b0fb130be3ff57618da34a37
matalie <natalie.stalder1@gmail.com>
2017-07-13 10:24:27 +0200

    blogcontrol

--- blog/src/AppBundle/Controller/BlogController.php
+++ blog/src/AppBundle/Controller/BlogController.php
@@ -26,9 +26,9 @@ class BlogController extends Controller {
         $otherBlogs = $query->getResult();
 
         return $this->render('showBlog.html.twig', [
-                    'title' => $user->getUserName(),
+                    'title' => $user->getBlog()->getBlogName(),
                     'posts' => $posts,
-                    'description' => $user->getDescription(),
+                    'description' => $user->getBlog()->getDescription(),
                     'searchBlogs' => $otherBlogs
         ]);
     }
@@ -62,6 +62,49 @@ class BlogController extends Controller {
             ]);
         }
         return $this->redirectToRoute('myblog');
+    }
+    
+     /**
+     * @Route("/editblog/{id}", name="editblog", requirements={"id": "\d+"})
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function editBlogAction($id, Request $request) {
+        $em = $this->get('doctrine')->getManager();
+
+        // get blog for userid
+        $query = $em->createQuery('SELECT b FROM AppBundle:Blog b WHERE b.id = ?1');
+        $query->setParameter(1, $id);
+        $results = $query->getResult();
+        
+        if (!empty($results)) {
+            $form = $this->createFormBuilder($results[0])
+                    ->add('blogname', TextType::class)
+                    ->add('description', TextareaType::class, array(
+                    'attr' => array(
+                        'class' => 'tinymce')
+                    ))
+                    ->add('save', SubmitType::class, array('label' => 'Submit'))
+                    ->getForm()
+            ;
+
+            $form->handleRequest($request);
+            $errors = $form->getErrors(true);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em = $this->get('doctrine')->getManager();
+                $blog = $form->getData();
+                $em->persist($blog);
+                $em->flush();
+                
+                return $this->redirectToRoute('myblog');
+            }
+
+            return $this->render('editBlog.html.twig', [
+                        'title' => "Edit Blog",
+                        'form' => $form->createView(),
+                        'errors' => null,
+            ]);
+        }
     }
 
 }
